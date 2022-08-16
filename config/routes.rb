48b1a53a27d61d2fd0306df17a0f9f5a096d77c3
Kenkaneki-42 <sylvain.vandermeersch@gmail.com>
2022-08-16 14:19:55 +0200

    add As a user, I can view the details of a task

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  # Read all // listing of tasks
+  get "tasks", to: "tasks#index"
+  # Read one
+  get "tasks/:id", to: "tasks#show", as: :task
   # root "articles#index"
 end
